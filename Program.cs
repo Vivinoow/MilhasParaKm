fd46b0c8ac322e128582a38dca0ff087046b0128
Viviane <vpereiradasilva555@gmail.com>
2022-09-17 10:36:41 -0300

    Continhas

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double milhas, kms;
+
+Console.Write("Entre coma a medida (em milhas):");
+milhas = Convert.ToDouble(Console.ReadLine());
+
+kms = milhas *1609;
+
+Console.WriteLine($"{kms:N3}Km");
